9a395d6840abd8c3cebaffc2aa7b56e353b0c1b6
Goga-NG <n.gogic01@gmail.com>
2026-05-21 19:14:50 +0200

    dodati indeksi za enzime

--- kreiraj.py
+++ kreiraj.py
@@ -15,6 +15,36 @@ fajlovi = [
 
 sadrzaj = "*Pitanje nije završeno*\n\n[← Nazad na pitanja](index.md)\n"
 
+folder = r"docs\druga-godina\medicinska-biohemija\enzimi"
+
+fajlovi = [
+    "opste-osobine-enzima-i-struktura-molekula-enzima.md",
+    "kvaternarna-struktura-enzima-izoenzimi.md",
+    "struktura-aktivnog-mesta-enzima-familije-enzima.md",
+    "nomenklatura-i-klasifikacija-enzima.md",
+    "mehanizmi-enzimske-katalize.md",
+    "kinetika-enzimske-katalize.md",
+    "inhibicija-enzima-bioloski-znacaj-reverzibilne-i-ireverzibilne-inhibicije-enzima.md",
+    "metali-i-oligoelementi-kao-kofaktori-enzima.md",
+    "multienzimski-kompleksi.md",
+    "subcelijska-organizacija-enzima.md",
+    "posttranslacioni-kovalentni-mehanizmi.md",
+    "posttranslacioni-nekovalentni-mehanizmi.md",
+    "regulacija-kolicine-enzima-u-celiji.md",
+    "alosterni-enzimi.md",
+    "koenzimi.md",
+    "ribozimi.md",
+    "funkcionalni-i-nefunkcionalni-enzimi-plazme.md",
+    "transaminaze.md",
+    "ggt.md",
+    "ck.md",
+    "ldh.md",
+    "amilaza.md",
+    
+]
+
+sadrzaj = "*Pitanje nije završeno*\n\n[← Nazad na pitanja](index.md)\n"
+
 for f in fajlovi:
     putanja = os.path.join(folder, f)
     with open(putanja, "w", encoding="utf-8") as file:
