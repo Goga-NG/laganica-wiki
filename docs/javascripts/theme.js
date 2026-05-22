119ed124270820554601be2c026a3bd7074f458d
Goga-NG <n.gogic01@gmail.com>
2026-05-22 22:02:16 +0200

    scroll to bottom

--- docs/javascripts/theme.js
+++ docs/javascripts/theme.js
@@ -2,3 +2,11 @@ var scheme = localStorage.getItem("data-md-color-scheme");
 if (scheme) {
   document.documentElement.setAttribute("data-md-color-scheme", scheme);
 }
+
+var btn = document.createElement("button");
+btn.className = "go-to-bottom";
+btn.innerHTML = "↓";
+btn.onclick = function() {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+};
+document.body.appendChild(btn);
